fdf37f03be1e88f650d4b8747216ac3d07ed1c6a
rafathossain <rafathossain96@gmail.com>
2023-07-08 16:01:54 +0600

    Updated worker file env

--- worker.py
+++ worker.py
@@ -1,14 +1,21 @@
 import os
 
 import redis
-from rq import Worker, Queue, Connection
+from dotenv import load_dotenv
+from rq import Worker
 
+# Loading variable from env file
+load_dotenv()
+
+# Worker queue
 listen = ['default']
 
-redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDIS_URL', 'redis://192.168.10.11:6379')
 
+# Creating redis connection
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
-    w = Worker(['default'], connection=conn)
+    # Starting RQ Worker
+    w = Worker(listen, connection=conn)
     w.work()
